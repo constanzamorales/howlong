e319a84e3c655b6e354475efbd3e61bb677d1f67
constanzamorales <constanzabmorales@gmail.com>
2022-10-12 20:02:14 -0300

    Added Luxon.js to display todays date formatted

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello");
+var DateTime = luxon.DateTime;
+let today = DateTime.now();
+let format = { month: "long", day: "numeric", year: "numeric" };
+today = today.setLocale("en-GB").toLocaleString(format);
+
+console.log(today);
+
+document.getElementById("date").innerHTML = today;
