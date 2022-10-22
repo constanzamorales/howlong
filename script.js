b29665800120f7f062be59de2b3063d678e88766
constanzamorales <constanzabmorales@gmail.com>
2022-10-22 11:39:48 -0300

    Selected button and start date input and added event listener to print stuff

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let format = { month: "long", day: "numeric", year: "numeric" };
 today = today.setLocale("en-GB").toLocaleString(format);
 
 console.log(today);
+
+const submitBtn = document.getElementById("submit-btn");
+const startDate = document.getElementById("start");
+
+submitBtn.addEventListener("click", (event) => {
+	event.preventDefault();
+	console.log("click");
+	console.log(startDate.value);
+});
