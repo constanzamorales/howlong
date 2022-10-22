b58afc21cd0097760ba3b01d7db25c688b4b3f78
constanzamorales <constanzabmorales@gmail.com>
2022-10-22 11:33:32 -0300

    Added Submit button and wrapped everything in a form element

--- script.js
+++ script.js
@@ -4,5 +4,3 @@ let format = { month: "long", day: "numeric", year: "numeric" };
 today = today.setLocale("en-GB").toLocaleString(format);
 
 console.log(today);
-
-document.getElementById("date").innerHTML = today;
