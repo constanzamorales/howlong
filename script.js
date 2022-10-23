4d6784a73b6a47eee14610e123bf41b2a92d8594
constanzamorales <constanzabmorales@gmail.com>
2022-10-23 12:52:06 -0300

    The Today button now prints todays date

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var DateTime = luxon.DateTime;
-let today = DateTime.now();
-let format = { month: "long", day: "numeric", year: "numeric" };
-today = today.setLocale("en-GB").toLocaleString(format);
-
-console.log(today);
-
 const submitBtn = document.getElementById("submit-btn");
 const startDate = document.getElementById("start");
+const todayBtn = document.getElementById("today-btn");
+
+todayBtn.addEventListener("click", (event) => {
+	event.preventDefault();
+	var DateTime = luxon.DateTime;
+	let today = DateTime.now();
+	console.log(today.toISODate());
+});
 
 submitBtn.addEventListener("click", (event) => {
 	event.preventDefault();
-	console.log("click");
 	console.log(startDate.value);
 });
