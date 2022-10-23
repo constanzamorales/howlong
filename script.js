9dd2c96674d754b522801e61434b3539bd753462
constanzamorales <constanzabmorales@gmail.com>
2022-10-23 12:58:41 -0300

    Submit button now prints end date. Today button needs some work

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const submitBtn = document.getElementById("submit-btn");
 const startDate = document.getElementById("start");
+const endDate = document.getElementById("end");
 const todayBtn = document.getElementById("today-btn");
 
 todayBtn.addEventListener("click", (event) => {
 	event.preventDefault();
 	var DateTime = luxon.DateTime;
-	let today = DateTime.now();
+	const today = DateTime.now();
 	console.log(today.toISODate());
 });
 
 submitBtn.addEventListener("click", (event) => {
 	event.preventDefault();
 	console.log(startDate.value);
+	console.log(endDate.value);
 });
