e5307773716893e42aca53cd72cdbb4ba6e454e4
constanzamorales <constanzabmorales@gmail.com>
2022-10-25 19:22:10 -0300

    Implemented a function to calculate months and days between two dates

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const submitBtn = document.getElementById("submit-btn");
-const startDate = document.getElementById("start");
-const endDate = document.getElementById("end");
+const startInput = document.getElementById("start");
+const endInput = document.getElementById("end");
 const todayBtn = document.getElementById("today-btn");
+const resultDisplay = document.getElementById("result");
 
 todayBtn.addEventListener("click", (event) => {
 	event.preventDefault();
@@ -12,6 +13,15 @@ todayBtn.addEventListener("click", (event) => {
 
 submitBtn.addEventListener("click", (event) => {
 	event.preventDefault();
-	console.log(startDate.value);
-	console.log(endDate.value);
+	console.log(startInput.value);
+	var DateTime = luxon.DateTime;
+	const startDate = DateTime.fromISO(startInput.value);
+	const endDate = DateTime.fromISO(endInput.value);
+	calculateResult(startDate, endDate);
 });
+
+const calculateResult = (start, end) => {
+	const result = end.diff(start, ["months", "days"]).toObject();
+	console.log(result);
+	return result;
+};
